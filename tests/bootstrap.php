1dc852b63729ce62c7b9c50e5a1a21237fb87b03
Jory Hogeveen <JoryHogeveen@users.noreply.github.com>
2016-08-02 16:29:21 +0200

    Update bootstrap.php

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -5,9 +5,9 @@ if ( ! $_tests_dir ) {
 	$_tests_dir = '/tmp/wordpress-tests-lib';
 }
 
-define('PLUGIN_NAME','view-admin-as.php');
-define('PLUGIN_FOLDER',basename(dirname( __DIR__ )));
-define('PLUGIN_PATH',PLUGIN_FOLDER.'/'.PLUGIN_NAME);
+define( 'PLUGIN_NAME'   , 'view-admin-as.php' );
+define( 'PLUGIN_FOLDER' , basename( dirname( __DIR__ ) ) );
+define( 'PLUGIN_PATH'   , PLUGIN_FOLDER.'/'.PLUGIN_NAME );
 
 // Activates this plugin in WordPress so it can be tested.
 $GLOBALS['wp_tests_options'] = array(
@@ -17,7 +17,7 @@ $GLOBALS['wp_tests_options'] = array(
 require_once $_tests_dir . '/includes/functions.php';
 
 function _manually_load_plugin() {
-	require dirname( __DIR__ ) . '/'.PLUGIN_NAME;
+	require dirname( __DIR__ ) . '/../'.PLUGIN_NAME;
 }
 
 tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
