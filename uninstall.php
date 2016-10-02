2a7d87651a01071371106570e6bfc49750da141a
Jory Hogeveen <jory.johan.hogeveen@gmail.com>
2016-10-03 01:40:21 +0200

    Improve uninstall for multisites

--- uninstall.php
+++ uninstall.php
@@ -3,10 +3,10 @@
  * View Admin As - Uninstaller
  *
  * Remove plugin data from the database
- * 
+ *
  * @author Jory Hogeveen <info@keraweb.nl>
  * @package view-admin-as
- * @version 1.5.3
+ * @version 1.5.x
  */
 
 //if uninstall not called from WordPress exit
@@ -14,9 +14,10 @@ if ( !defined( 'WP_UNINSTALL_PLUGIN' ) ) {
     exit();
 }
 
-if ( ! is_multisite() ) {
-	vaa_uninstall();
-} else {
+
+vaa_uninstall();
+
+if ( is_multisite() ) {
 	global $wp_version;
 	if ( version_compare( $wp_version, '4.5.999', '<' ) ) {
 		// Sadly does not work for large networks -> return false
@@ -27,30 +28,45 @@ if ( ! is_multisite() ) {
 	if ( $blogs ) {
 		foreach ( $blogs as $blog ) {
 			$blog = (array) $blog;
-			switch_to_blog( intval( $blog['blog_id'] ) );
-			vaa_uninstall();
+			vaa_uninstall( intval( $blog['blog_id'] ) );
 		}
-		restore_current_blog();
+		vaa_uninstall( 'site' );
 	}
 }
+function vaa_uninstall( $blog_id = false ) {
 
-function vaa_uninstall() {
-	
 	// Delete all View Admin As options
 	$option_keys = array( 'vaa_view_admin_as', 'vaa_role_defaults' );
-	foreach ( $option_keys as $option_key ) {
-		delete_option( $option_key );
-	}
-	
-	// Delete all View Admin As user metadata
-	$user_meta_keys = array( 'vaa-view-admin-as' );
-	// Older (not used anymore) keys
-	$user_meta_keys[] = 'view-admin-as';
-	$users = get_users();
-	foreach ( $users as $user ) {
-		foreach ( $user_meta_keys as $user_meta_key ) {
-			delete_user_meta( $user->ID, $user_meta_key );
+
+	if ( $blog_id ) {
+
+		if ( $blog_id == 'site' ) {
+			foreach ( $option_keys as $option_key ) {
+				delete_site_option( $option_key );
+			}
+		} else {
+			foreach ( $option_keys as $option_key ) {
+				delete_blog_option( $blog_id, $option_key );
+			}
+		}
+
+	} else {
+
+		foreach ( $option_keys as $option_key ) {
+			delete_option( $option_key );
+		}
+
+		// Delete all View Admin As user metadata
+		$user_meta_keys = array( 'vaa-view-admin-as' );
+		// Older (not used anymore) keys
+		$user_meta_keys[] = 'view-admin-as';
+
+		global $wpdb;
+		$all_users = $wpdb->get_results("SELECT ID FROM $wpdb->users");
+		foreach ( $all_users as $user ) {
+			foreach ( $user_meta_keys as $user_meta_key ) {
+				delete_user_meta( $user->ID, $user_meta_key );
+			}
 		}
 	}
-	
 }
