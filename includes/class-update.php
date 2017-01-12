d05d8d44b743cf5d08be4a8701484ccef7bfd234
Jory Hogeveen <jory.johan.hogeveen@gmail.com>
2017-01-12 20:32:18 +0100

    get db version from store (compat) + refactor method order

--- includes/class-update.php
+++ includes/class-update.php
@@ -36,6 +36,21 @@ final class VAA_View_Admin_As_Update extends VAA_View_Admin_As_Class_Base
 	}
 
 	/**
+	 * Check the correct DB version in the DB
+	 *
+	 * @since   1.4
+	 * @since   1.6    Moved to this class from main class
+	 * @access  public
+	 * @return  void
+	 */
+	public function maybe_db_update() {
+		$db_version = strtolower( $this->store->get_optionData('db_version') );
+		if ( version_compare( $db_version, $this->store->get_dbVersion(), '<' ) ) {
+			$this->db_update();
+		}
+	}
+
+	/**
 	 * Update settings
 	 *
 	 * @since   1.4
@@ -45,7 +60,7 @@ final class VAA_View_Admin_As_Update extends VAA_View_Admin_As_Class_Base
 	 */
 	private function db_update() {
 		$defaults = array(
-			'db_version' => VIEW_ADMIN_AS_DB_VERSION,
+			'db_version' => $this->store->get_dbVersion(),
 		);
 
 		$current_db_version = strtolower( $this->store->get_optionData('db_version') );
@@ -62,27 +77,12 @@ final class VAA_View_Admin_As_Update extends VAA_View_Admin_As_Class_Base
 		}
 
 		// Update version, append if needed
-		$this->store->set_optionData( VIEW_ADMIN_AS_DB_VERSION, 'db_version', true );
+		$this->store->set_optionData( $this->store->get_dbVersion(), 'db_version', true );
 		// Update option data
 		$this->store->update_optionData( wp_parse_args( $this->store->get_optionData(), $defaults ) );
 
 		// Main update finished, hook used to update modules
 		do_action( 'vaa_view_admin_as_db_update' );
-	}
-
-	/**
-	 * Check the correct DB version in the DB
-	 *
-	 * @since   1.4
-	 * @since   1.6    Moved to this class from main class
-	 * @access  public
-	 * @return  void
-	 */
-	public function maybe_db_update() {
-		$db_version = strtolower( $this->store->get_optionData('db_version') );
-		if ( version_compare( $db_version, $this->store->get_dbVersion(), '<' ) ) {
-			$this->db_update();
-		}
 	}
 
 	/**
