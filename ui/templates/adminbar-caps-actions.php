2b6851f3d096d55e3caab5a2c7446a910b699ecf
Jory Hogeveen <jory.johan.hogeveen@gmail.com>
2017-02-13 15:41:08 +0100

    Var validation to prevent wrong usage

--- ui/templates/adminbar-caps-actions.php
+++ ui/templates/adminbar-caps-actions.php
@@ -2,108 +2,127 @@
 /**
  * Add caps actions.
  *
+ * @since  1.6.x
+ *
  * @var  WP_Admin_Bar  $admin_bar  The toolbar object.
  * @var  string        $root       The current root item.
  * @var  string        $main_root  The main VAA root item.
  */
 
-$admin_bar->add_node(
-	array(
-		'id'     => $root . '-filtercaps',
-		'parent' => $root . '-select',
-		'title'  => VAA_View_Admin_As_Admin_Bar::do_input(
-			array(
-				'name'        => $root . '-filtercaps',
-				'placeholder' => esc_attr__( 'Filter', VIEW_ADMIN_AS_DOMAIN ),
-			)
-		),
-		'href'   => false,
-		'meta'   => array(
-			'class' => 'ab-vaa-filter filter-caps vaa-column-one-half vaa-column-first',
-		),
-	)
-);
+! defined( 'VIEW_ADMIN_AS_DIR' ) and die( 'You shall not pass!' );
 
-$role_select_options = array(
-	array(
-		'value' => 'default',
-		'label' => __( 'Default', VIEW_ADMIN_AS_DOMAIN ),
-	),
-);
-if ( $this->store->get_view() ) {
-	$data_caps = wp_json_encode( $this->store->get_selectedCaps() );
-	$role_select_options[] = array(
-		'compare' => 'vaa',
-		'label'   => '= ' . __( 'Current view', VIEW_ADMIN_AS_DOMAIN ),
-		'attr'    => array(
-			'data-caps' => $data_caps,
-		),
-	);
-	$role_select_options[] = array(
-		'compare' => 'reversed-vaa',
-		'label'   => '≠ ' . __( 'Current view', VIEW_ADMIN_AS_DOMAIN ),
-		'attr'    => array(
-			'data-caps'    => $data_caps,
-			'data-reverse' => '1',
-		),
-	);
-}
-foreach ( $this->store->get_roles() as $role_key => $role ) {
-	$data_caps = wp_json_encode( $role->capabilities );
-	$role_select_options[] = array(
-		'compare' => esc_attr( $role_key ),
-		'label'   => '= ' . $this->store->get_rolenames( $role_key ),
-		'attr'    => array(
-			'data-caps' => $data_caps,
-		),
-	);
-	$role_select_options[] = array(
-		'compare' => 'reversed-' . esc_attr( $role_key ),
-		'label'   => '≠ ' . $this->store->get_rolenames( $role_key ),
-		'attr'    => array(
-			'data-caps'    => $data_caps,
-			'data-reverse' => '1',
-		),
-	);
-}
-$admin_bar->add_node(
-	array(
-		'id'     => $root . '-selectrolecaps',
-		'parent' => $root . '-select',
-		'title'  => VAA_View_Admin_As_Admin_Bar::do_select(
-			array(
-				'name'   => $root . '-selectrolecaps',
-				'values' => $role_select_options,
-			)
-		),
-		'href'   => false,
-		'meta'   => array(
-			'class' => 'ab-vaa-select select-role-caps vaa-column-one-half vaa-column-last',
-			'html'  => '',
-		),
-	)
-);
+if ( isset( $admin_bar ) && $admin_bar instanceof WP_Admin_Bar && isset( $root ) ) {
 
-$admin_bar->add_node(
-	array(
-		'id'     => $root . '-bulkselectcaps',
-		'parent' => $root . '-select',
-		'title'  => VAA_View_Admin_As_Admin_Bar::do_button(
-			array(
-				'name'    => 'select-all-caps',
-				'label'   => __( 'Select', VIEW_ADMIN_AS_DOMAIN ),
-				'classes' => 'button-secondary',
-			)
-		) . ' ' . VAA_View_Admin_As_Admin_Bar::do_button(
-			array(
-				'name'    => 'deselect-all-caps',
-				'label'   => __( 'Deselect', VIEW_ADMIN_AS_DOMAIN ),
-				'classes' => 'button-secondary',
-			)
+	if ( ! isset( $main_root ) ) {
+		$main_root = $root;
+	}
+
+	// Text filter
+	$admin_bar->add_node(
+		array(
+			'id'     => $root . '-filtercaps',
+			'parent' => $root . '-select',
+			'title'  => VAA_View_Admin_As_Admin_Bar::do_input(
+				array(
+					'name'        => $root . '-filtercaps',
+					'placeholder' => esc_attr__( 'Filter', VIEW_ADMIN_AS_DOMAIN ),
+				)
+			),
+			'href'   => false,
+			'meta'   => array(
+				'class' => 'ab-vaa-filter filter-caps vaa-column-one-half vaa-column-first',
+			),
+		)
+	);
+
+	// Select filter
+	$role_select_options = array(
+		array(
+			'value' => 'default',
+			'label' => __( 'Default', VIEW_ADMIN_AS_DOMAIN ),
 		),
-		'href'   => false,
-		'meta'   => array(
-			'class' => 'vaa-button-container vaa-clear-float',
-		),
-	)
-);
+	);
+	// View filter
+	if ( $this->store->get_view() ) {
+		$data_caps = wp_json_encode( $this->store->get_selectedCaps() );
+		$role_select_options[] = array(
+			'compare' => 'vaa',
+			'label'   => '= ' . __( 'Current view', VIEW_ADMIN_AS_DOMAIN ),
+			'attr'    => array(
+				'data-caps' => $data_caps,
+			),
+		);
+		$role_select_options[] = array(
+			'compare' => 'reversed-vaa',
+			'label'   => '≠ ' . __( 'Current view', VIEW_ADMIN_AS_DOMAIN ),
+			'attr'    => array(
+				'data-caps'    => $data_caps,
+				'data-reverse' => '1',
+			),
+		);
+	}
+	// Role filters
+	foreach ( $this->store->get_roles() as $role_key => $role ) {
+		$data_caps = wp_json_encode( $role->capabilities );
+		$role_select_options[] = array(
+			'compare' => esc_attr( $role_key ),
+			'label'   => '= ' . $this->store->get_rolenames( $role_key ),
+			'attr'    => array(
+				'data-caps' => $data_caps,
+			),
+		);
+		$role_select_options[] = array(
+			'compare' => 'reversed-' . esc_attr( $role_key ),
+			'label'   => '≠ ' . $this->store->get_rolenames( $role_key ),
+			'attr'    => array(
+				'data-caps'    => $data_caps,
+				'data-reverse' => '1',
+			),
+		);
+	}
+	$admin_bar->add_node(
+		array(
+			'id'     => $root . '-selectrolecaps',
+			'parent' => $root . '-select',
+			'title'  => VAA_View_Admin_As_Admin_Bar::do_select(
+				array(
+					'name'   => $root . '-selectrolecaps',
+					'values' => $role_select_options,
+				)
+			),
+			'href'   => false,
+			'meta'   => array(
+				'class' => 'ab-vaa-select select-role-caps vaa-column-one-half vaa-column-last',
+				'html'  => '',
+			),
+		)
+	);
+
+	// Select/deselect
+	$admin_bar->add_node(
+		array(
+			'id'     => $root . '-bulkselectcaps',
+			'parent' => $root . '-select',
+			'title'  => VAA_View_Admin_As_Admin_Bar::do_button(
+				array(
+					'name'    => 'select-all-caps',
+					'label'   => __( 'Select', VIEW_ADMIN_AS_DOMAIN ),
+					'classes' => 'button-secondary',
+				)
+			) . ' ' . VAA_View_Admin_As_Admin_Bar::do_button(
+				array(
+					'name'    => 'deselect-all-caps',
+					'label'   => __( 'Deselect', VIEW_ADMIN_AS_DOMAIN ),
+					'classes' => 'button-secondary',
+				)
+			),
+			'href'   => false,
+			'meta'   => array(
+				'class' => 'vaa-button-container vaa-clear-float',
+			),
+		)
+	);
+
+} else {
+	_doing_it_wrong( __FILE__, esc_html__( 'No toolbar resources found.', VIEW_ADMIN_AS_DOMAIN ), '1.6.x' );
+}
--- ui/templates/adminbar-caps-items.php
+++ ui/templates/adminbar-caps-items.php
@@ -2,50 +2,64 @@
 /**
  * Add caps items.
  *
+ * @since  1.6.x
+ *
  * @var  WP_Admin_Bar  $admin_bar  The toolbar object.
  * @var  string        $root       The current root item.
  * @var  string        $main_root  The main VAA root item.
  */
 
-$caps_items = '';
-foreach ( $this->store->get_caps() as $cap_name => $cap_val ) {
-	$class = 'vaa-cap-item';
-	$checked = false;
-	// check if we've selected a capability view and we've changed some capabilities.
-	$selected_caps = $this->store->get_view( 'caps' );
-	if ( isset( $selected_caps[ $cap_name ] ) ) {
-		if ( 1 === (int) $selected_caps[ $cap_name ] ) {
+! defined( 'VIEW_ADMIN_AS_DIR' ) and die( 'You shall not pass!' );
+
+if ( isset( $admin_bar ) && $admin_bar instanceof WP_Admin_Bar && isset( $root ) ) {
+
+	if ( ! isset( $main_root ) ) {
+		$main_root = $root;
+	}
+
+	$caps_items = '';
+	foreach ( $this->store->get_caps() as $cap_name => $cap_val ) {
+		$class   = 'vaa-cap-item';
+		$checked = false;
+		// check if we've selected a capability view and we've changed some capabilities.
+		$selected_caps = $this->store->get_view( 'caps' );
+		if ( isset( $selected_caps[ $cap_name ] ) ) {
+			if ( 1 === (int) $selected_caps[ $cap_name ] ) {
+				$checked = true;
+			}
+		} elseif ( 1 === (int) $cap_val ) {
 			$checked = true;
 		}
-	} elseif ( 1 === (int) $cap_val ) {
-		$checked = true;
-	}
-	// Check for this capability in any view set.
-	if ( $this->vaa->view()->current_view_can( $cap_name ) ) {
-		$class .= ' current';
-	}
-	// The list of capabilities.
-	$caps_items .=
-		'<div class="ab-item ' . $class . '">'
-		. VAA_View_Admin_As_Admin_Bar::do_checkbox(
-			array(
-				'name'           => 'vaa_cap_' . esc_attr( $cap_name ),
-				'value'          => $checked,
-				'compare'        => true,
-				'checkbox_value' => esc_attr( $cap_name ),
-				'label'          => $cap_name,
+		// Check for this capability in any view set.
+		if ( $this->vaa->view()->current_view_can( $cap_name ) ) {
+			$class .= ' current';
+		}
+		// The list of capabilities.
+		$caps_items .=
+			'<div class="ab-item ' . $class . '">'
+			. VAA_View_Admin_As_Admin_Bar::do_checkbox(
+				array(
+					'name'           => 'vaa_cap_' . esc_attr( $cap_name ),
+					'value'          => $checked,
+					'compare'        => true,
+					'checkbox_value' => esc_attr( $cap_name ),
+					'label'          => $cap_name,
+				)
 			)
+			. '</div>';
+	}
+	$admin_bar->add_node(
+		array(
+			'id'     => $root . '-select-options',
+			'parent' => $root . '-select',
+			'title'  => $caps_items,
+			'href'   => false,
+			'meta'   => array(
+				'class' => 'ab-vaa-multipleselect auto-height',
+			),
 		)
-		. '</div>';
+	);
+
+} else {
+	_doing_it_wrong( __FILE__, esc_html__( 'No toolbar resources found.', VIEW_ADMIN_AS_DOMAIN ), '1.6.x' );
 }
-$admin_bar->add_node(
-	array(
-		'id'     => $root . '-select-options',
-		'parent' => $root . '-select',
-		'title'  => $caps_items,
-		'href'   => false,
-		'meta'   => array(
-			'class' => 'ab-vaa-multipleselect auto-height',
-		),
-	)
-);
