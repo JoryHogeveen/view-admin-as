449b7eda895140022b193d821171a915a571810b
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2017-05-03 00:08:12 +0200

    Add resource (include parent) validation in settings template

--- ui/templates/adminbar-settings-user.php
+++ ui/templates/adminbar-settings-user.php
@@ -16,121 +16,102 @@
  * - force_group_users
  */
 
-/**
- * admin_menu_location setting.
- *
- * @since   1.5
- */
-$admin_bar->add_node(
-	array(
-		'id'     => $root . '-admin-menu-location',
-		'parent' => $root,
-		'title'  => VAA_View_Admin_As_Form::do_select(
-			array(
-				'name'        => $root . '-admin-menu-location',
-				'value'       => $this->store->get_userSettings( 'admin_menu_location' ),
-				'label'       => __( 'Location', VIEW_ADMIN_AS_DOMAIN ) . ': &nbsp; ',
-				'description' => __( 'Change the location of this menu node', VIEW_ADMIN_AS_DOMAIN ),
-				'values'      => array(
-					array(
-						'compare' => 'top-secondary',
-						'label' => __( 'Default', VIEW_ADMIN_AS_DOMAIN ),
-					),
-					array(
-						'compare' => 'my-account',
-						'label' => __( 'My account', VIEW_ADMIN_AS_DOMAIN ),
-					),
-				),
-				//'auto_showhide_desc' => true
-			)
-		),
-		'href'   => false,
-		'meta'   => array(
-			'class' => 'auto-height',
-		),
-	)
-);
+if ( ! defined( 'VIEW_ADMIN_AS_DIR' ) ) {
+	die();
+}
 
-/**
- * view_mode setting.
- *
- * @since   1.5
- */
-$admin_bar->add_node(
-	array(
-		'id'     => $root . '-view-mode',
-		'parent' => $root,
-		'title'  => VAA_View_Admin_As_Form::do_radio(
-			array(
-				'name'     => $root . '-view-mode',
-				'value'    => $this->store->get_userSettings( 'view_mode' ),
-				'values'   => array(
-					array(
-						'compare'     => 'browse',
-						'label'       => __( 'Browse mode', VIEW_ADMIN_AS_DOMAIN ),
-						'description' => __( 'Store view and use WordPress with this view', VIEW_ADMIN_AS_DOMAIN ),
-					),
-					array(
-						'compare'     => 'single',
-						'label'       => __( 'Single switch mode', VIEW_ADMIN_AS_DOMAIN ),
-						'description' => __( 'Choose view on every pageload. This setting doesn\'t store views', VIEW_ADMIN_AS_DOMAIN ),
-					),
-				),
-				//'auto_showhide_desc' => true
-			)
-		),
-		'href'   => false,
-		'meta'   => array(
-			'class' => 'auto-height',
-		),
-	)
-);
+if ( isset( $this ) &&
+	 isset( $this->store ) &&
+	 isset( $admin_bar ) && $admin_bar instanceof WP_Admin_Bar &&
+	 isset( $root )
+) {
 
-/**
- * hide_front setting.
- *
- * @since   1.6
- */
-$admin_bar->add_node(
-	array(
-		'id'     => $root . '-hide-front',
-		'parent' => $root,
-		'title'  => VAA_View_Admin_As_Form::do_checkbox(
-			array(
-				'name'        => $root . '-hide-front',
-				'value'       => $this->store->get_userSettings( 'hide_front' ),
-				'compare'     => 'yes',
-				'label'       => __( 'Hide on frontend', VIEW_ADMIN_AS_DOMAIN ),
-				'description' => __( 'Hide on frontend when no view is selected and the admin bar is not shown', VIEW_ADMIN_AS_DOMAIN ),
-				//'auto_showhide_desc' => true,
-			)
-		),
-		'href'   => false,
-		'meta'   => array(
-			'class' => 'auto-height',
-		),
-	)
-);
-
-/**
- * freeze_locale setting.
- * Force own locale on view, WP 4.7+ only.
- *
- * @see     https://github.com/JoryHogeveen/view-admin-as/issues/21
- * @since   1.6.1
- */
-if ( VAA_API::validate_wp_version( '4.7' ) ) {
+	/**
+	 * admin_menu_location setting.
+	 *
+	 * @since   1.5
+	 */
 	$admin_bar->add_node(
 		array(
-			'id'     => $root . '-freeze-locale',
+			'id'     => $root . '-admin-menu-location',
+			'parent' => $root,
+			'title'  => VAA_View_Admin_As_Form::do_select(
+				array(
+					'name'        => $root . '-admin-menu-location',
+					'value'       => $this->store->get_userSettings( 'admin_menu_location' ),
+					'label'       => __( 'Location', VIEW_ADMIN_AS_DOMAIN ) . ': &nbsp; ',
+					'description' => __( 'Change the location of this menu node', VIEW_ADMIN_AS_DOMAIN ),
+					'values'      => array(
+						array(
+							'compare' => 'top-secondary',
+							'label'   => __( 'Default', VIEW_ADMIN_AS_DOMAIN ),
+						),
+						array(
+							'compare' => 'my-account',
+							'label'   => __( 'My account', VIEW_ADMIN_AS_DOMAIN ),
+						),
+					),
+					//'auto_showhide_desc' => true
+				)
+			),
+			'href'   => false,
+			'meta'   => array(
+				'class' => 'auto-height',
+			),
+		)
+	);
+
+	/**
+	 * view_mode setting.
+	 *
+	 * @since   1.5
+	 */
+	$admin_bar->add_node(
+		array(
+			'id'     => $root . '-view-mode',
+			'parent' => $root,
+			'title'  => VAA_View_Admin_As_Form::do_radio(
+				array(
+					'name'   => $root . '-view-mode',
+					'value'  => $this->store->get_userSettings( 'view_mode' ),
+					'values' => array(
+						array(
+							'compare'     => 'browse',
+							'label'       => __( 'Browse mode', VIEW_ADMIN_AS_DOMAIN ),
+							'description' => __( 'Store view and use WordPress with this view', VIEW_ADMIN_AS_DOMAIN ),
+						),
+						array(
+							'compare'     => 'single',
+							'label'       => __( 'Single switch mode', VIEW_ADMIN_AS_DOMAIN ),
+							'description' => __( 'Choose view on every pageload. This setting doesn\'t store views', VIEW_ADMIN_AS_DOMAIN ),
+						),
+					),
+					//'auto_showhide_desc' => true
+				)
+			),
+			'href'   => false,
+			'meta'   => array(
+				'class' => 'auto-height',
+			),
+		)
+	);
+
+	/**
+	 * hide_front setting.
+	 *
+	 * @since   1.6
+	 */
+	$admin_bar->add_node(
+		array(
+			'id'     => $root . '-hide-front',
 			'parent' => $root,
 			'title'  => VAA_View_Admin_As_Form::do_checkbox(
 				array(
-					'name'        => $root . '-freeze-locale',
-					'value'       => $this->store->get_userSettings( 'freeze_locale' ),
+					'name'        => $root . '-hide-front',
+					'value'       => $this->store->get_userSettings( 'hide_front' ),
 					'compare'     => 'yes',
-					'label'       => __( 'Freeze locale', VIEW_ADMIN_AS_DOMAIN ),
-					'description' => __( 'Force your own locale setting to the current view', VIEW_ADMIN_AS_DOMAIN ),
+					'label'       => __( 'Hide on frontend', VIEW_ADMIN_AS_DOMAIN ),
+					'description' => __( 'Hide on frontend when no view is selected and the admin bar is not shown', VIEW_ADMIN_AS_DOMAIN ),
 					//'auto_showhide_desc' => true,
 				)
 			),
@@ -140,33 +121,66 @@ if ( VAA_API::validate_wp_version( '4.7' ) ) {
 			),
 		)
 	);
-}
 
-/**
- * force_group_users setting.
- *
- * @since   1.5.2
- */
-if ( ! $this->groupUserRoles || 15 >= ( count( $this->store->get_users() ) + count( $this->store->get_roles() ) ) ) {
-	$admin_bar->add_node(
-		array(
-			'id'     => $root . '-force-group-users',
-			'parent' => $root,
-			'title'  => VAA_View_Admin_As_Form::do_checkbox(
-				array(
-					'name'        => $root . '-force-group-users',
-					'value'       => $this->store->get_userSettings( 'force_group_users' ),
-					'compare'     => 'yes',
-					'label'       => __( 'Group users', VIEW_ADMIN_AS_DOMAIN ),
-					'description' => __( 'Group users under their assigned roles', VIEW_ADMIN_AS_DOMAIN ),
-					//'auto_showhide_desc' => true,
-				)
-			),
-			'href'   => false,
-			'meta'   => array(
-			'class'    => 'auto-height',
-				'tabindex' => '0',
-			),
-		)
-	);
+	/**
+	 * freeze_locale setting.
+	 * Force own locale on view, WP 4.7+ only.
+	 *
+	 * @see     https://github.com/JoryHogeveen/view-admin-as/issues/21
+	 * @since   1.6.1
+	 */
+	if ( VAA_API::validate_wp_version( '4.7' ) ) {
+		$admin_bar->add_node(
+			array(
+				'id'     => $root . '-freeze-locale',
+				'parent' => $root,
+				'title'  => VAA_View_Admin_As_Form::do_checkbox(
+					array(
+						'name'        => $root . '-freeze-locale',
+						'value'       => $this->store->get_userSettings( 'freeze_locale' ),
+						'compare'     => 'yes',
+						'label'       => __( 'Freeze locale', VIEW_ADMIN_AS_DOMAIN ),
+						'description' => __( 'Force your own locale setting to the current view', VIEW_ADMIN_AS_DOMAIN ),
+						//'auto_showhide_desc' => true,
+					)
+				),
+				'href'   => false,
+				'meta'   => array(
+					'class' => 'auto-height',
+				),
+			)
+		);
+	}
+
+	/**
+	 * force_group_users setting.
+	 *
+	 * @since   1.5.2
+	 */
+	if ( empty( $this->groupUserRoles ) ||
+	     15 >= ( count( $this->store->get_users() ) + count( $this->store->get_roles() ) )
+	) {
+		$admin_bar->add_node(
+			array(
+				'id'     => $root . '-force-group-users',
+				'parent' => $root,
+				'title'  => VAA_View_Admin_As_Form::do_checkbox(
+					array(
+						'name'        => $root . '-force-group-users',
+						'value'       => $this->store->get_userSettings( 'force_group_users' ),
+						'compare'     => 'yes',
+						'label'       => __( 'Group users', VIEW_ADMIN_AS_DOMAIN ),
+						'description' => __( 'Group users under their assigned roles', VIEW_ADMIN_AS_DOMAIN ),
+						//'auto_showhide_desc' => true,
+					)
+				),
+				'href'   => false,
+				'meta'   => array(
+					'class'    => 'auto-height',
+					'tabindex' => '0',
+				),
+			)
+		);
+	}
+
 }
