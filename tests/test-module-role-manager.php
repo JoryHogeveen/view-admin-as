21edde025c9cd9f0163f7eeff17ced57fb5374a3
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2017-08-23 00:32:12 +0200

    Unit tests for Role Manager

---
+++ tests/test-module-role-manager.php
@@ -0,0 +1,226 @@
+<?php
+/**
+ * View Admin As - Unit tests
+ *
+ * Module: Role Manager
+ *
+ * @todo Save methods + existing roles vs new roles
+ *
+ * @author  Jory Hogeveen <info@keraweb.nl>
+ * @package View_Admin_As
+ */
+
+view_admin_as()->include_file( VIEW_ADMIN_AS_DIR . 'modules/class-role-manager.php', 'VAA_View_Admin_As_Role_Manager' );
+
+class VAA_Module_Role_Manager_UnitTest extends WP_UnitTestCase {
+
+	/**
+	 * @return VAA_View_Admin_As_Role_Manager
+	 */
+	static function get_instance() {
+		return VAA_View_Admin_As_Role_Manager::get_instance();
+	}
+
+	/**
+	 * Tests for:
+	 * - sanitize_role_name
+	 * - sanitize_role_slug
+	 */
+	function test_sanitize() {
+		$class = self::get_instance();
+
+		/**
+		 * Sanitize role names
+		 */
+
+		// Capitalize
+		$this->assertEquals( 'Test', $class::sanitize_role_name( 'test' ) );
+		// Capitalize and convert underscores to spaces
+		$this->assertEquals( 'Test Yay', $class::sanitize_role_name( 'test_yay' ) );
+
+		/**
+		 * Sanitize role slugs
+		 */
+
+		// Remove caps
+		$this->assertEquals( 'test', $class::sanitize_role_slug( 'Test' ) );
+		// Special chars, keep underscores
+		$this->assertEquals( 'test', $class::sanitize_role_slug( 'Test@_' ) );
+		// Lowercase and underscores only
+		$this->assertEquals( 'test_yay', $class::sanitize_role_slug( 'Test@_Yay!' ) );
+		$this->assertEquals( 'test_yay', $class::sanitize_role_slug( 'Test Yay!' ) );
+
+	}
+
+	/**
+	 * Test export roles
+	 */
+	function test_export() {
+		$class = self::get_instance();
+
+		$export = $class->export_roles( array( 'role' => '__all__' ) );
+		$roles = array_keys( $export );
+		$compare = array(
+			'administrator',
+			'editor',
+			'author',
+			'contributor',
+			'subscriber',
+		);
+
+		$this->assertEquals( $roles, $compare );
+
+		$export = $class->export_roles( array( 'role' => 'editor' ) );
+
+		$roles = array_keys( $export );
+		$compare = array( 'editor' );
+
+		$this->assertEquals( $roles, $compare );
+
+		$export = $class->export_roles( array( 'role' => 'non_existing_role' ) );
+		$compare = __( 'Role not found', VIEW_ADMIN_AS_DOMAIN );
+
+		$this->assertEquals( $export, $compare );
+
+	}
+
+	/**
+	 * Test import roles
+	 */
+	function test_import() {
+		$class = self::get_instance();
+		$role = 'test_import';
+
+		/**
+		 * Incorrect.
+		 */
+		$result = $class->import_roles( 'test' );
+		// We expect an error string.
+		$this->assertNotEquals( true, $result );
+
+		$caps = array(
+			'read' => array( 'yay' ),
+			'test' => true,
+		);
+		$result = $class->import_roles( $caps );
+		// We expect an error array.
+		$this->assertNotEquals( true, $result );
+
+		/**
+		 * Correct.
+		 */
+		$caps = array(
+			'read' => true,
+			'test' => true,
+		);
+
+		$result = $class->import_roles( array(
+			'data' => array( $role => $caps ),
+		) );
+		$this->assertEquals( true, $result );
+
+		$test_import = get_role( $role );
+
+		$this->assertEquals( $caps, $test_import->capabilities );
+	}
+
+	/**
+	 * Test clone roles
+	 */
+	function test_clone() {
+		$class = self::get_instance();
+		$role = 'test_clone';
+
+		/**
+		 * Incorrect.
+		 */
+		$result = $class->clone_role( 'non_existing_role', $role );
+		// We expect an error string.
+		$this->assertNotEquals( true, $result );
+
+		/**
+		 * Correct.
+		 */
+		$result = $class->clone_role( 'editor', $role );
+		// We expect an error string.
+		$this->assertEquals( true, $result );
+
+		$editor = get_role( 'editor' );
+		$test_clone = get_role( $role );
+
+		$this->assertEquals( $editor->capabilities, $test_clone->capabilities );
+
+	}
+
+	/**
+	 * Test rename roles
+	 */
+	function test_rename() {
+		$class = self::get_instance();
+		$rename = 'Rename';
+
+		/**
+		 * Incorrect.
+		 */
+		$result = $class->rename_role( 'non_existing_role', $rename );
+		// We expect an error string.
+		$this->assertNotEquals( true, $result );
+
+		/**
+		 * Correct.
+		 */
+		$result = $class->rename_role( 'editor', $rename );
+		// We expect an error string.
+		$this->assertEquals( true, $result );
+
+		$editor = get_role( 'editor' );
+		// The editor should not exist anymore.
+		$this->assertEquals( $editor->name, $rename );
+
+		// Revert change
+		$class->rename_role( 'editor', 'Editor' );
+		$editor = get_role( 'editor' );
+		// The editor should not exist anymore.
+		$this->assertEquals( $editor->name, 'Editor' );
+
+	}
+
+	/**
+	 * Test delete roles
+	 */
+	function test_delete() {
+		$class = self::get_instance();
+		// Load all roles again.
+		VAA_UnitTest_Factory::vaa_reinit();
+
+		/**
+		 * Incorrect.
+		 */
+		$result = $class->delete_role( 'non_existing_role' );
+		// We expect an error string.
+		$this->assertNotEquals( true, $result );
+
+		/**
+		 * Protected roles.
+		 */
+		$result = $class->delete_role( 'administrator' );
+		// We expect an error string.
+		$this->assertNotEquals( true, $result );
+
+		$result = $class->delete_role( get_option( 'default_role' ) );
+		// We expect an error string.
+		$this->assertNotEquals( true, $result );
+
+		/**
+		 * Correct.
+		 */
+		$result = $class->delete_role( 'test_import' );
+		$this->assertEquals( true, $result );
+
+		$result = $class->delete_role( 'test_clone' );
+		$this->assertEquals( true, $result );
+
+		// Load all roles again after removal.
+		VAA_UnitTest_Factory::vaa_reinit();
+	}
+}
