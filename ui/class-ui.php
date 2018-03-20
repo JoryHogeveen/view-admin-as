8b94155b042aa4feaaae8b5c84eb3e807fdb867d
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2018-03-20 16:56:38 +0100

    Missed version tag

--- ui/class-ui.php
+++ ui/class-ui.php
@@ -17,7 +17,7 @@ if ( ! defined( 'VIEW_ADMIN_AS_DIR' ) ) {
  * @package View_Admin_As
  * @since   1.6
  * @since   1.7  Renamed from VAA_View_Admin_As_Admin
- * @version 1.7.6
+ * @version 1.7.6.1
  * @uses    VAA_View_Admin_As_Base Extends class
  */
 final class VAA_View_Admin_As_UI extends VAA_View_Admin_As_Base
