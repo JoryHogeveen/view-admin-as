e790d2a29f013e6427cbdd0d072668aa0a121c44
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2018-07-04 01:39:56 +0200

    Add search-by selectbox for user search when ajax is enabled

--- ui/templates/adminbar-user-actions.php
+++ ui/templates/adminbar-user-actions.php
@@ -34,13 +34,31 @@ if ( isset( $admin_bar ) && $admin_bar instanceof WP_Admin_Bar && isset( $root )
 		if ( $this->group_user_roles() ) {
 			$title = VAA_View_Admin_As_Form::do_description( __( 'Users are grouped under their roles', VIEW_ADMIN_AS_DOMAIN ) );
 		}
+		if ( $this->ajax_search() ) {
+			$title .= VAA_View_Admin_As_Form::do_select( array(
+				'name'   => $root . '-searchusers-by',
+				'values' => array(
+					''             => ' - ' . __( 'Search by', VIEW_ADMIN_AS_DOMAIN ) . ' - ',
+					'ID'           => 'ID',
+					'user_login'   => __( 'Username', VIEW_ADMIN_AS_DOMAIN ),
+					'user_email'   => __( 'Email', VIEW_ADMIN_AS_DOMAIN ),
+					'user_url'     => __( 'Website', VIEW_ADMIN_AS_DOMAIN ),
+					'display_name' => __( 'Display name', VIEW_ADMIN_AS_DOMAIN ),
+					//'user_nicename'
+				),
+				'class'  => 'vaa-wide',
+			) );
+		}
+		$title .= VAA_View_Admin_As_Form::do_input( array(
+			'name'        => $root . '-searchusers',
+			'placeholder' => __( 'Search', VIEW_ADMIN_AS_DOMAIN ),
+			'class'       => 'vaa-wide',
+		) );
+
 		$admin_bar->add_node( array(
 			'id'     => $root . '-searchusers',
 			'parent' => $root,
-			'title'  => $title . VAA_View_Admin_As_Form::do_input( array(
-				'name'        => $root . '-searchusers',
-				'placeholder' => __( 'Search', VIEW_ADMIN_AS_DOMAIN ),
-			) ),
+			'title'  => $title,
 			'href'   => false,
 			'meta'   => array(
 				'class' => 'ab-vaa-search search-users' . ( ( $this->ajax_search() ) ? ' search-ajax' : '' ),
