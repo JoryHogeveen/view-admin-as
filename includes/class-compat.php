7e10c5c7abbd9a78695d79b2eb5b2dec6a868e21
Jory Hogeveen <JoryHogeveen@users.noreply.github.com>
2020-05-13 16:10:46 +0200

    Prevent other filters from overriding our full caps list

--- includes/class-compat.php
+++ includes/class-compat.php
@@ -394,10 +394,10 @@ final class VAA_View_Admin_As_Compat extends VAA_View_Admin_As_Base
 			$caps = array_merge( (array) members_get_plugin_capabilities(), $caps );
 		}
 		// Get caps from multiple plugins through the Members filter.
-		$caps = apply_filters( 'members_get_capabilities', $caps );
+		$caps = array_merge( apply_filters( 'members_get_capabilities', array() ), $caps );
 
 		// Pods.
-		$caps = apply_filters( 'pods_roles_get_capabilities', $caps );
+		$caps = array_merge( apply_filters( 'pods_roles_get_capabilities', array() ), $caps );
 
 		return $caps;
 
